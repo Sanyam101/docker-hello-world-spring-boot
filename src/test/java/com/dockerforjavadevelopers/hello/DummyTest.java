c1579779d52ec6e860aee9d4ce2fc92ea5ace584
root <root@pipelinedemo.c.intrepid-signal-195508.internal>
2018-03-23 10:56:35 +0000

    test

--- src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
+++ src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
@@ -13,35 +13,4 @@ public class DummyTest {
     assertEquals(true, true);
   }
 
-@Test
-public void bTest(){
-String url="http://localhost:8082/";
-		String[] status={"curl", "-s"," -o", "/dev/null", "-w", "%{http_code}",url};
-		ProcessBuilder process = new ProcessBuilder(status); 
-        Process p;	
-        try
-        {
-            p = process.start();
-             BufferedReader reader =  new BufferedReader(new InputStreamReader(p.getInputStream()));
-                StringBuilder builder = new StringBuilder();
-                String line = null;
-                while ( (line = reader.readLine()) != null) {
-                        builder.append(line);
-                        builder.append(System.getProperty("line.separator"));
-                }
-                String result = builder.toString();
-                System.out.print(result);
-                
-
-                if(result=="200"){
-                	System.out.println("deployment is succesfull");
-                }else{
-                	System.out.println("deployment not successfull");
-                }
-        }
-        catch (IOException e)
-        {   System.out.print("error");
-            e.printStackTrace();
-        }
-}
 }
