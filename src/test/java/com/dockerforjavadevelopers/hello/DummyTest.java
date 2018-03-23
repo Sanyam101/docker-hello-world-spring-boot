0b081fbb3792ae55485f68b5f8e4ba97afe2f02e
root <root@pipelinedemo.c.intrepid-signal-195508.internal>
2018-03-23 10:51:19 +0000

    added test

--- src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
+++ src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
@@ -1,7 +1,9 @@
 package com.dockerforjavadevelopers.hello;
 
 import static org.junit.Assert.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.junit.Test;
 
 public class DummyTest {
@@ -10,4 +12,36 @@ public class DummyTest {
   public void aTest() {
     assertEquals(true, true);
   }
+
+@Test
+public void bTest(){
+String url="http://localhost:8082/";
+		String[] status={"curl", "-s"," -o", "/dev/null", "-w", "%{http_code}",url};
+		ProcessBuilder process = new ProcessBuilder(status); 
+        Process p;	
+        try
+        {
+            p = process.start();
+             BufferedReader reader =  new BufferedReader(new InputStreamReader(p.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ( (line = reader.readLine()) != null) {
+                        builder.append(line);
+                        builder.append(System.getProperty("line.separator"));
+                }
+                String result = builder.toString();
+                System.out.print(result);
+                
+
+                if(result=="200"){
+                	System.out.println("deployment is succesfull");
+                }else{
+                	System.out.println("deployment not successfull");
+                }
+        }
+        catch (IOException e)
+        {   System.out.print("error");
+            e.printStackTrace();
+        }
+}
 }
